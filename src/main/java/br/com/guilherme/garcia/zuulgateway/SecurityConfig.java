6a6bc8b3e19e2a388c2e5a6eb41de59886e6a75c
Guilherme Garcia <guilherme.garcia86@gmail.com>
2018-10-14 20:22:55 -0300

    Add cloud foundry config

---
+++ src/main/java/br/com/guilherme/garcia/zuulgateway/SecurityConfig.java
@@ -0,0 +1,16 @@
+package br.com.guilherme.garcia.zuulgateway;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().anyRequest().permitAll();
+
+	}
+
+}
